3e7cc2ad0cd21bf5a1af066f6bd5dc1f97bdb5f2
ShaydurovaEkaterina <ka10a@mail.ru>
2016-04-08 09:59:29 +0400

    read all users, read a little of comments, encoding utf-8

--- P-2016.py
+++ P-2016.py
@@ -4,9 +4,11 @@ import codecs
 class User:
     displayname = ''
     age = -1
-    def new_user(self, name, age):
+    id = -1
+    def new_user(self, name, age, id):
         self.displayname = name
         self.age = age
+        self.id = id
 
 def del_space(s):
     a = []
@@ -19,39 +21,59 @@ def del_space(s):
         i += 1
     return a
 
+def is_digit(s):
+    a  = 0
 
-fout = open("test.txt", "w")
+#fout = open("test.txt", "w")
 fout2 = open("test2.txt", "w")
 fout3 = open("test3.txt", "w")
-f_users = codecs.open('users.xml', encoding='cp1251')
+fout4 = open("test4.txt", "w")
+f_users = codecs.open('Users.xml', encoding='utf-8')
+f_comm = codecs.open('Comments.xml', encoding='cp1251')
 # 'cp1251')
 
 users = []
-#for s in f_users.readlines():
-for i in range(100):
-    s = f_users.readline()
+for s in f_users.readlines():
+#for i in range(10):
+    #s = f_users.readline()
+    #print(s, file=fout)
     a = del_space(s)
     name = ''
     age = -1
     #print(a, file=fout2)
     for elem in a:
-        print(elem[:3], file=fout2)
+        #print(elem[:3], file=fout2)
+        #if (elem[])
         if (elem[:11] == 'DisplayName'):
             name = elem[13:-1]
         if (elem[:3] == 'Age'):
             age = elem[5:-1]
-            age = int(age)
-            print(age)
+            #print(age, file=fout2)
+            #print(age.isdigit(), file=fout2)
+            if (age.isdigit()):
+                age = int(age)
+            else:
+                age = -1
+            #print(age, file=fout2)
 
-    if ((19 > age) or (age > 26) or (name == '')):
+    if ((name == '') or (age == -1)):
         continue
 
-    x = User()
-    x.new_user(name, age)
-    users.append(x)
+    if (20 <= age) and (age <= 25):
+        x = User()
+        x.new_user(name, age, 0)
+        users.append(x)
 
-print(len(users), file=fout3)
+#print(len(users), file=fout3)
+#for elem in users:
+#    print(elem.age, file=fout3)
 
 #for i in range(100):
-#    s = f_users.readline()
-#    print(s, file=fout)
+    #s = f_users.readline()
+    #print(s, file=fout)
+
+#print(len(users))
+
+for i in range(100):
+    s = f_comm.readline()
+    print(s, file=fout4)
