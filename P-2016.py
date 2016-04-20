feba7191b1752497b17cc0c0b78f07a9d3825416
ShaydurovaEkaterina <ka10a@mail.ru>
2016-04-20 19:34:27 +0400

    change links, codestyle

--- P-2016.py
+++ P-2016.py
@@ -9,17 +9,15 @@ f_comm = codecs.open('Comments.xml', encoding='utf-8')
 
 
 class User:
-    displayname = ''
+    d_name = ''
     age = None
-    id = None
+    id_ = None
     comm = 0
-    link = ''
 
-    def __init__(self, name, age, _id, link):
-        self.displayname = name
+    def __init__(self, name, age, id_):
+        self.d_name = name
         self.age = age
-        self.id = _id
-        self.link = link
+        self.id_ = id_
         self.comm = 0
 
 
@@ -41,24 +39,20 @@ def read_users():
         a = del_space(s)
         name = ''
         age = None
-        _id = None
-        link = ''
+        id_ = None
 
         for elem in a:
             if elem.startswith('Id'):
-                _id = elem[4:-1]
+                id_ = elem[4:-1]
 
-                if _id.isdigit():
-                    _id = int(_id)
+                if id_.isdigit():
+                    id_ = int(id_)
                 else:
-                    _id = None
+                    id_ = None
                     break
 
             if elem.startswith('DisplayName'):
                 name = elem[13:-1]
-
-            if elem.startswith('WebsiteUrl'):
-                link = elem[12:-1]
 
             if elem.startswith('Age'):
                 age = elem[5:-1]
@@ -69,28 +63,27 @@ def read_users():
                     age = None
                     break
 
-        if (name == '') or (age is None) or (_id is None):
+        if (name == '') or (age is None) or (id_ is None):
             continue
 
         if (20 <= age) and (age <= 25):
-            new = User(name, age, _id, link)
-            users[_id] = new
+            new = User(name, age, id_)
+            users[id_] = new
 
-    #print(len(users))
     return users
 
 
 def read_comments(users):
     for s in f_comm.readlines():
         a = del_space(s)
-        _id = 0
+        id_ = 0
 
         for elem in a:
             if elem.startswith('UserId'):
-                _id = int(elem[8:-1])
+                id_ = int(elem[8:-1])
 
-        if _id in users:
-            users[_id].comm += 1
+        if id_ in users:
+            users[id_].comm += 1
 
     return users
 
@@ -100,14 +93,10 @@ def generate_table(users, rate):
     print(temp.TABLE_TEMPLATE, file=fout)
 
     for i in range(150):
-        table_user = User()
         table_user = users[rate[i][1]]
         print("<tr>", file=fout)
         print("<th>{0}</th>".format(i + 1), file=fout)
-        if table_user.link != "":
-            print("<th> <a href='{0}'</a>".format(table_user.link),  table_user.displayname, "</th>", sep='', file=fout)
-        else:
-            print("<th>{0}</th>".format(table_user.displayname), file=fout)
+        print("<th> <a href='https://electronics.stackexchange.com/users/{0}'</a>".format(table_user.id_),  table_user.d_name, "</th>", sep='', file=fout)
         print("<th>{0}</th>".format(table_user.age), file=fout)
         print("<th>{0}</th>".format(table_user.comm), file=fout)
         print("</tr>", file=fout)
@@ -118,8 +107,8 @@ def generate_table(users, rate):
 _users = read_comments(read_users())
 
 _rate = []
-for _id in _users.keys():
-    _rate.append((_users[_id].comm, int(_id)))
+for id_ in _users.keys():
+    _rate.append((_users[id_].comm, int(id_)))
 _rate.sort(reverse=True)
 
 generate_table(_users, _rate)
